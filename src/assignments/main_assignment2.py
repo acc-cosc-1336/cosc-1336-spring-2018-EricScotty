60f521429670d3bfaee7db3cc3f6f594299b1c9f
EricScotty <35587124+EricScotty@users.noreply.github.com>
2018-01-30 22:09:05 -0600

    Add files via upload

---
+++ src/assignments/main_assignment2.py
@@ -0,0 +1,7 @@
+from assignment2 import faculty_evaluation_result
+
+'''Write code to call the faculty_evaluation_result function with data of your choice'''
+result = faculty_evaluation_result ( 1000, 5, 10, 25, 35, 100)
+print(result)
+
+
